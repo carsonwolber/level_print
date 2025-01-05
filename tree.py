e4b0507787ecf16e6f7113ce3be1743aee8c4361
carsonwolber <ctw54@cornell.edu>
2025-01-05 17:48:00 -0500

    added traversal code and helper

--- tree.py
+++ tree.py
@@ -1,4 +1,5 @@
 from node import Node
+import collections
 
 
 class Tree:
@@ -6,6 +7,28 @@ class Tree:
     self.root = root
 
   def print_level_traversal(self):
-    pass
+    print(self.root.val, "\n")
+
+    Q = collections.deque()
+    Q.append(self.root)
+
+    while Q: 
+      level = []
+      for _ in range(len(Q)):
+        n = Q.popleft()
+
+        if n.right:
+          Q.append(n.right)
+        if n.left:
+          Q.append(n.left)
+      self.print_level_helper(level)
+  
+  def print_level_helper(self, level):
+    print(l + ' ' for l in level)
+    print("\n")
+
+
+        
+
 
 
